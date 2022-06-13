28f3f9b76bc3386381ad868ac65d98ff8fa03901
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-06-13 10:44:47 +0800

    work video background

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,18 +1,28 @@
 import Image from "next/image";
 import React from "react";
-import videoBG from "../public/videos/circuit.mp4";
+import Typed from "react-typed";
 
 const HeroSection = () => {
   return (
     <section className="relative bg-white">
-      <video loop autoPlay muted className="heroBackground" src={videoBG} />
-      <Image
+      <video
+        loop
+        autoPlay
+        muted
+        className="object-cover h-full w-full"
+        // src={require("/circuit.mp4")}
+        src="/circuit.mp4"
+        type="video/mp4"
+      />
+      {/* <source /> */}
+      {/* </video> */}
+      {/* <Image
         className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
         src="https://images.unsplash.com/photo-1601758003122-53c40e686a19"
         alt="Couple on a bed with a dog"
         layout="fill"
-      />
-      <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent" />
+      /> */}
+      <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white-100 sm:to-transparent" />
       <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
         <div className="max-w-xl text-center sm:text-left">
           <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
@@ -21,7 +31,17 @@ const HeroSection = () => {
           </h1>
 
           <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
-            I&apos;m a Full Stack Web Developer
+            I&apos;m a{" "}
+            <Typed
+              strings={[
+                "Full Stack Web Developer",
+                "Web Designer",
+                "Wordpress Expert",
+              ]}
+              typeSpeed={100}
+              backSpeed={50}
+              loop
+            />
           </p>
           <div className="flex flex-wrap gap-4 mt-8 text-center">
             <a
--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
+const withVideos = require("next-videos");
+module.exports = withVideos();
+
 module.exports = {
   images: {
     domains: ["images.unsplash.com"],
