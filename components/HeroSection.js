53812d50d82bb45b7845de0bf94ccb88fbb9d95d
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-06-13 14:38:58 +0800

    remove overlay

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -14,13 +14,6 @@ const HeroSection = () => {
         type="video/mp4"
       />
 
-      {/* <Image
-        className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
-        src="https://images.unsplash.com/photo-1601758003122-53c40e686a19"
-        alt="Couple on a bed with a dog"
-        layout="fill"
-      /> */}
-      <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-gray-300 sm:to-transparent" />
       <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
         <div className="max-w-xl text-center sm:text-left">
           <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
