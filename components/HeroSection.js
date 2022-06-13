169c517c291ed1b6a448151b8bf88d711154f853
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-06-13 10:49:56 +0800

    video bg

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -9,20 +9,18 @@ const HeroSection = () => {
         loop
         autoPlay
         muted
-        className="object-cover h-full w-full"
-        // src={require("/circuit.mp4")}
+        className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
         src="/circuit.mp4"
         type="video/mp4"
       />
-      {/* <source /> */}
-      {/* </video> */}
+
       {/* <Image
         className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
         src="https://images.unsplash.com/photo-1601758003122-53c40e686a19"
         alt="Couple on a bed with a dog"
         layout="fill"
       /> */}
-      <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white-100 sm:to-transparent" />
+      <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-gray-300 sm:to-transparent" />
       <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
         <div className="max-w-xl text-center sm:text-left">
           <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
@@ -30,7 +28,7 @@ const HeroSection = () => {
             <span className="sm:block">Lester Reandino</span>
           </h1>
 
-          <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
+          <p className="max-w-lg mt-4 text-white text-2xl sm:leading-relaxed sm:text-xl">
             I&apos;m a{" "}
             <Typed
               strings={[
@@ -45,7 +43,7 @@ const HeroSection = () => {
           </p>
           <div className="flex flex-wrap gap-4 mt-8 text-center">
             <a
-              className="block w-full px-12 py-3 text-sm font-medium text-black border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring"
+              className="block w-full px-12 py-3 text-lg font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring"
               href="/about"
             >
               Learn More
