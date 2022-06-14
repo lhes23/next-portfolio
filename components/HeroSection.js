1b90e2fafbfeebbb9394f00ee43e9f92aab61fbc
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-06-14 21:41:49 +0800

    heading name centered

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -17,8 +17,8 @@ const HeroSection = ({ personalDetails, professionalDetails }) => {
         type="video/mp4"
       />
 
-      <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
-        <div className="max-w-xl text-center sm:text-left">
+      <div className="relative w-full px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
+        <div className="w-full text-center justify-center">
           <h1 className="text-3xl font-extrabold text-transparent sm:text-7xl bg-clip-text bg-gradient-to-r from-green-300 via-pink-600 to-purple-600 backgroundAnimate shadow-md">
             Hi, my name is
             <span className="sm:block">
@@ -26,16 +26,19 @@ const HeroSection = ({ personalDetails, professionalDetails }) => {
             </span>
           </h1>
 
-          <p className="max-w-lg mt-4 text-2xl sm:leading-relaxed sm:text-4xl text-white">
+          <p className="mt-4 text-2xl sm:leading-relaxed sm:text-4xl text-white text-center">
             I&apos;m a{" "}
-            <Typed
-              strings={jobTitles.map((title) => title)}
-              typeSpeed={100}
-              backSpeed={50}
-              loop
-            />
+            <span className="block">
+              <Typed
+                strings={jobTitles.map((title) => title)}
+                typeSpeed={100}
+                backSpeed={50}
+                loop
+                className="text-left"
+              />
+            </span>
           </p>
-          <div className="flex flex-wrap gap-4 mt-8 text-center">
+          <div className="flex flex-wrap gap-4 mt-8 text-center justify-center">
             <a
               className="block w-full px-12 py-3 text-lg font-medium text-white border border-white rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring"
               href="/about"
