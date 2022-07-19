9a7c2b4d23f61f039bab96f8d08ad821275ada91
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-19 12:53:28 +0800

    add colors to icons

--- components/Icons.js
+++ components/Icons.js
@@ -16,31 +16,31 @@ const Icons = ({ icon }) => {
   const size = 40;
   switch (icon) {
     case "HTML":
-      return <AiOutlineHtml5 size={size} />;
+      return <AiOutlineHtml5 size={size} style={{ color: "#dd4d25" }} />;
     case "CSS":
-      return <FaCss3 size={size} />;
+      return <FaCss3 size={size} style={{ color: "#146eb0" }} />;
     case "Javascript":
-      return <SiJavascript size={size} />;
+      return <SiJavascript size={size} style={{ color: "#f7df1e" }} />;
     case "PHP":
-      return <SiPhp size={size} />;
+      return <SiPhp size={size} style={{ color: "#7377ad" }} />;
     case "WordPress":
-      return <SiWordpress size={size} />;
+      return <SiWordpress size={size} style={{ color: "#207196" }} />;
     case "Python":
-      return <SiPython size={size} />;
+      return <SiPython size={size} style={{ color: "#356a97" }} />;
     case "ReactJS":
-      return <SiReact size={size} />;
+      return <SiReact size={size} style={{ color: "#5ed3f3" }} />;
     case "NextJS":
-      return <SiNextdotjs size={size} />;
+      return <SiNextdotjs size={size} style={{ color: "#000" }} />;
     case "cPanel":
-      return <SiCpanel size={size} />;
+      return <SiCpanel size={size} style={{ color: "#f7692b" }} />;
     case "PhpMyAdmin":
-      return <SiPhpmyadmin size={size} />;
+      return <SiPhpmyadmin size={size} style={{ color: "#f0970e" }} />;
     case "Web Host Manager":
-      return <FaWhmcs size={size} />;
+      return <FaWhmcs size={size} style={{ color: "#ea692f" }} />;
     case "NodeJS":
-      return <FaNodeJs size={size} />;
+      return <FaNodeJs size={size} style={{ color: "green" }} />;
     case "ExpressJS":
-      return <SiExpress style={{ backgroundColor: "red" }} size={size} />;
+      return <SiExpress style={{ color: "black" }} size={size} />;
   }
 };
 
