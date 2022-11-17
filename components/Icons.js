346bdecbf19d888c74d8521c70189432493c6768
Lester <32542078+lhes23@users.noreply.github.com>
2022-11-17 20:03:23 +0800

    add 2 new skills - edit on the data.json

--- components/Icons.js
+++ components/Icons.js
@@ -1,5 +1,5 @@
-import { AiOutlineHtml5 } from "react-icons/ai";
-import { FaCss3, FaNodeJs, FaWhmcs } from "react-icons/fa";
+import { AiOutlineHtml5, AiFillApi } from "react-icons/ai"
+import { FaCss3, FaNodeJs, FaWhmcs } from "react-icons/fa"
 import {
   SiJavascript,
   SiPhp,
@@ -10,38 +10,43 @@ import {
   SiCpanel,
   SiPhpmyadmin,
   SiExpress,
-} from "react-icons/si";
+  SiDjango
+} from "react-icons/si"
 
 const Icons = ({ icon }) => {
-  const size = 40;
+  const size = 40
   switch (icon) {
     case "HTML":
-      return <AiOutlineHtml5 size={size} style={{ color: "#dd4d25" }} />;
+      return <AiOutlineHtml5 size={size} style={{ color: "#dd4d25" }} />
     case "CSS":
-      return <FaCss3 size={size} style={{ color: "#146eb0" }} />;
+      return <FaCss3 size={size} style={{ color: "#146eb0" }} />
     case "Javascript":
-      return <SiJavascript size={size} style={{ color: "#f7df1e" }} />;
+      return <SiJavascript size={size} style={{ color: "#f7df1e" }} />
     case "PHP":
-      return <SiPhp size={size} style={{ color: "#7377ad" }} />;
+      return <SiPhp size={size} style={{ color: "#7377ad" }} />
     case "WordPress":
-      return <SiWordpress size={size} style={{ color: "#207196" }} />;
+      return <SiWordpress size={size} style={{ color: "#207196" }} />
     case "Python":
-      return <SiPython size={size} style={{ color: "#356a97" }} />;
+      return <SiPython size={size} style={{ color: "#356a97" }} />
     case "ReactJS":
-      return <SiReact size={size} style={{ color: "#5ed3f3" }} />;
+      return <SiReact size={size} style={{ color: "#5ed3f3" }} />
     case "NextJS":
-      return <SiNextdotjs size={size} style={{ color: "#000" }} />;
+      return <SiNextdotjs size={size} style={{ color: "#000" }} />
     case "cPanel":
-      return <SiCpanel size={size} style={{ color: "#f7692b" }} />;
+      return <SiCpanel size={size} style={{ color: "#f7692b" }} />
     case "PhpMyAdmin":
-      return <SiPhpmyadmin size={size} style={{ color: "#f0970e" }} />;
+      return <SiPhpmyadmin size={size} style={{ color: "#f0970e" }} />
     case "Web Host Manager":
-      return <FaWhmcs size={size} style={{ color: "#ea692f" }} />;
+      return <FaWhmcs size={size} style={{ color: "#ea692f" }} />
     case "NodeJS":
-      return <FaNodeJs size={size} style={{ color: "green" }} />;
+      return <FaNodeJs size={size} style={{ color: "green" }} />
     case "ExpressJS":
-      return <SiExpress style={{ color: "black" }} size={size} />;
+      return <SiExpress style={{ color: "black" }} size={size} />
+    case "Django":
+      return <SiDjango style={{ color: "green" }} size={size} />
+    case "Django Rest Framework":
+      return <AiFillApi style={{ color: "green" }} size={size} />
   }
-};
+}
 
-export default Icons;
+export default Icons
