9dd28e0acd6c0fe1d2fbf2eaae1c19c5014c7cc3
Lester <32542078+lhes23@users.noreply.github.com>
2022-11-20 11:14:06 +0800

    add env

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,31 @@
-import "../styles/globals.css";
-import App from "next/app";
-import baseUrl from "../utils/baseUrl";
-import { createContext, useState } from "react";
-import data from "../utils/data.json";
+import "../styles/globals.css"
+import App from "next/app"
+import baseUrl from "../utils/baseUrl"
+import { createContext, useState } from "react"
+import data from "../utils/data.json"
 
-export const UserContext = createContext();
+export const UserContext = createContext()
 
 function MyApp({ Component, pageProps }) {
-  console.log("_app - userDetails: ", pageProps.userDetails);
+  console.log(process.env.NODE_ENV)
   // const [userState, setUserState] = useState(pageProps.userDetails);
-  const [userState, setUserState] = useState(data);
+  const [userState, setUserState] = useState(data)
 
   return (
     <UserContext.Provider value={userState}>
       <Component {...pageProps} />
     </UserContext.Provider>
-  );
+  )
 }
 
 MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
+  const appProps = await App.getInitialProps(appContext)
+  console.log(baseUrl)
+  const res = await fetch(`${baseUrl}/api/user`)
+  const data = await res.json()
+  appProps.pageProps = { userDetails: data.userDetails[0] }
 
-  const res = await fetch(`${baseUrl}/api/user`);
-  const data = await res.json();
-  appProps.pageProps = { userDetails: data.userDetails[0] };
+  return { ...appProps }
+}
 
-  return { ...appProps };
-};
-
-export default MyApp;
+export default MyApp
