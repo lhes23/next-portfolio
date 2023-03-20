5403b36014969ca908c1a3d9e8cf01e4ca6f08bb
Lester <32542078+lhes23@users.noreply.github.com>
2023-03-21 07:37:06 +0800

    minimize footer font text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   const { firstName, lastName } = ctx.personalDetails
   return (
     <div className="w-full justify-center text-center p-4 bg-white">
-      <p className="text-slate-600 p-4">
+      <p className="text-slate-600 p-4 text-xs">
         &copy;Copyright {new Date().getFullYear()} || All Rights Reserved. ||{" "}
         {firstName} {lastName}
       </p>
