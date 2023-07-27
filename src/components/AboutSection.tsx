dea77bb1175998fac2e65bdae334de124a90a9e5
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-27 14:24:09 +0800

    remove extra comments from about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -46,44 +46,6 @@ const AboutSection = () => {
           </motion.div>
         </div>
       </div>
-      {/* <div className="py-8" ref={jobRef}>
-        <h2 className="text-2xl font-bold text-white sm:text-3xl justify-center align-center flex">
-          Job Experiences
-        </h2>
-        <motion.div
-          className="grid lg:gap-4 justify-center align-center sm:grid-cols-2 grid-cols-1 gap-2 py-8 lg:px-4"
-          variants={container}
-          initial="hidden"
-          animate={jobInView ? "visible" : ""}
-        >
-          {jobExperiences.map((job) => {
-            return (
-              <motion.div
-                className="card w-full glass"
-                key={job.title}
-                variants={item}
-              >
-                <div className="card-body text-white">
-                  <h2 className="card-title">{job.title}</h2>
-                  <span className="text-sm text-gray-50">
-                    {job.date} - {job.company}
-                  </span>
-                  <ul className="list-disc">
-                    {job.tasks.map((task) => {
-                      return (
-                        <li className="py-2" key={task}>
-                          {task}
-                        </li>
-                      );
-                    })}
-                  </ul>
-                  <div className="card-actions justify-end"></div>
-                </div>
-              </motion.div>
-            );
-          })}
-        </motion.div>
-      </div> */}
     </section>
   )
 }
