b4b3e9b33788be22f755074ed8a6d3b54d6291a6
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-28 10:32:10 +0800

    add bg blur on contact

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -12,8 +12,8 @@ const ContactSection = () => {
   return (
     <>
       <section className={styles.section} id="contact">
-        <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
-          <div className="lg:py-8 lg:col-span-2">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-5">
+          <div className="p-8 lg:col-span-2 backdrop-blur-md bg-white/30 rounded-lg">
             <h3 className="font-bold text-center text-xl py-4 lg:text-2xl lg:p-10">
               I would like to connect
             </h3>
