d797dcb37b7b1e5191d3a8e12357bbd2b59ee39a
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-28 10:29:52 +0800

    add spacing on about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -23,7 +23,7 @@ const AboutSection = () => {
         </div>
         <div ref={summaryRef}>
           <motion.div
-            className="lg:py-24 backdrop-blur-md bg-white/30"
+            className="p-10 backdrop-blur-md bg-white/30 rounded-lg"
             variants={leftElement}
             initial="hidden"
             animate={summaryInView ? "visible" : "hidden"}
