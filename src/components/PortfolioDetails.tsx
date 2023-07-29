24080acebcdfbe4fd50f4b8deccb5386961f279e
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-29 23:59:02 +0800

    add bg on portfolio details

--- src/components/PortfolioDetails.tsx
+++ src/components/PortfolioDetails.tsx
@@ -2,6 +2,8 @@
 import React from "react"
 import Link from "next/link"
 import { IPortfolio } from "@/utils/interfaces"
+import Image from "next/image"
+import { styles } from "./MainContent"
 
 const PortFolioDetails = ({
   id,
@@ -39,6 +41,47 @@ const PortFolioDetails = ({
           </div>
         </div>
       </div>
+      <section
+        className={`${styles.section} h-screen bg-[url('/images/bg-laptop.jpg')] bg-cover bg-no-repeat bg-fixed`}
+      >
+        <div className="m-auto px-6">
+          <div className="lg:flex justify-between items-center">
+            <div className={`lg:w-6/12 p-7 ${styles.bgCardGlass}`}>
+              <h1 className="text-4xl font-bold leading-tight mb-5 capitalize">
+                {name}
+              </h1>
+              <p className="text-xl">{description}</p>
+              <div className="py-5">
+                <a
+                  href="#"
+                  className="text-white rounded-full py-2 px-5 text-lg font-semibold bg-purple-600 inline-block border border-purple-600 mr-3"
+                >
+                  Try for free
+                </a>
+                <a
+                  href="#"
+                  className="text-black rounded-full py-2 px-5 text-lg font-semibold bg-gray-400 inline-block border hover:bg-white hover:text-black"
+                >
+                  Requist a demo
+                </a>
+              </div>
+            </div>
+            <div className="lg:w-5/12 order-2">
+              <Image
+                src={`/images/portfolio/${img}`}
+                width={1000}
+                height={1000}
+                alt={name}
+                // style={{
+                //   transform:
+                //     "scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg) rotate(2deg)"
+                // }}
+                className="rounded shadow-lg"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
