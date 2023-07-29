9ba8fea04232dfd969bdcac490c35e780aea3958
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-29 21:14:31 +0800

    fix the portfolio details with redirect to 404

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -1,16 +1,22 @@
-"use client"
 import React from "react"
-import { notFound, useParams } from "next/navigation"
+import { notFound } from "next/navigation"
 import PortFolioDetails from "@/components/PortfolioDetails"
-import ctx from "@/utils/data.json"
+import data from "@/utils/data.json"
 
-const PortfolioPage = () => {
-  const { portfolios } = ctx
-  const { id } = useParams()
+const fetchPortfolios = async (id: string) => {
+  const { portfolios } = await data
   const portfolio = portfolios.find((p) => p.id === id)
-  return (
-    <>{portfolio ? <PortFolioDetails portfolio={portfolio} /> : notFound()}</>
-  )
+  return portfolio ? portfolio : notFound()
+}
+
+const PortfolioPage = async ({ params }: { params: { id: string } }) => {
+  const { id } = params
+  const portfolio = await fetchPortfolios(id)
+  if (!portfolio) {
+    notFound()
+  }
+
+  return <PortFolioDetails {...portfolio} />
 }
 
 export default PortfolioPage
--- src/components/PortfolioDetails.tsx
+++ src/components/PortfolioDetails.tsx
@@ -3,24 +3,32 @@ import React from "react"
 import Link from "next/link"
 import { IPortfolio } from "@/utils/interfaces"
 
-const PortFolioDetails = ({ portfolio }: { portfolio: IPortfolio }) => {
+const PortFolioDetails = ({
+  id,
+  name,
+  url,
+  img,
+  app,
+  description,
+  createdAt
+}: IPortfolio) => {
   return (
     <>
       <div
         className="hero min-h-screen"
-        style={{ backgroundImage: `url(/images/portfolio/${portfolio?.img})` }}
+        style={{ backgroundImage: `url(/images/portfolio/${img})` }}
       >
         <div className="hero-overlay bg-opacity-80"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">{portfolio?.name}</h1>
-            <p className="mb-5 text-2xl">{portfolio?.description}</p>
+            <h1 className="mb-5 text-5xl font-bold">{name}</h1>
+            <p className="mb-5 text-2xl">{description}</p>
             <div className="flex justify-evenly">
               <Link href="/#portfolios" className="btn btn-accent">
                 Back to Portfolios
               </Link>
               <a
-                href={portfolio?.url}
+                href={url}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn btn-primary"
