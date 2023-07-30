5d7e7d4d4d53eb2e643ab2df1f158e42ca1c8325
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-30 20:50:49 +0800

    add experimental on next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["three"]
+  transpilePackages: ["three"],
+  experimental: {
+    nftTracing: true
+  }
 }
 
 module.exports = nextConfig
