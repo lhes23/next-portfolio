eb6ca46649273603def78603d87992c178294ec0
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-30 15:10:51 +0800

    fetch data from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,11 @@ import React from "react"
 import MainContent from "@/components/MainContent"
 import baseUrl from "@/utils/baseUrl"
 
-export const fetchData = async () => {
+const Home = async () => {
   const res = await fetch(`${baseUrl}/api/staticdata/`)
   const data = await res.json()
-  return data
+  if (!data) return <>Cant Load Data</>
+  return <MainContent data={data} />
 }
 
-export default async function Home() {
-  const objectData = await fetchData()
-  if (!objectData) return <>Cant Load Data</>
-  return <MainContent data={objectData} />
-}
+export default Home
