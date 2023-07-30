f8209fb9e96e5ee98f8c41be5c3e7a26bbdcc9a2
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-30 16:09:40 +0800

    remove fetch and get data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import MainContent from "@/components/MainContent"
 import baseUrl from "@/utils/baseUrl"
+import objectData from "@/utils/data.json"
 
 const Home = async () => {
   try {
-    const res = await fetch(`${baseUrl}/api/staticdata/`)
-    const data = await res.json()
+    // const res = await fetch(`${baseUrl}/api/staticdata/`)
+    // const data = await res.json()
+    const data = objectData
     return <MainContent data={data} />
   } catch (error) {
     console.log(error)
