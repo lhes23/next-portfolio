c5a3bb117a5499c71865f2fdc7bf393f7d1eea74
Lester <32542078+lhes23@users.noreply.github.com>
2023-07-30 11:05:30 +0800

    update nav

--- src/components/JobExperiences.tsx
+++ src/components/JobExperiences.tsx
@@ -14,7 +14,7 @@ const JobExperiences = ({
   const isInView = useInView(jobRef)
 
   return (
-    <section className={styles.section}>
+    <section className={styles.section} id="experiences">
       <div className="py-8">
         <div className="justify-center p-4 m-4 text-center">
           <h2 className={styles.title}>Job Experiences</h2>
