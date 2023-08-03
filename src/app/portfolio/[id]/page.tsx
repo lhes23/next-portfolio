98081bbce40622c9fe68517eb07a87e68c0227ae
Lester <32542078+lhes23@users.noreply.github.com>
2023-08-03 13:30:28 +0800

    fix single portfolio

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import { notFound } from "next/navigation"
 import PortFolioDetails from "@/components/PortfolioDetails"
 import data from "@/utils/data.json"
+import { fetchData } from "@/hooks/fetchData"
+import baseUrl from "@/utils/baseUrl"
 
 const fetchPortfolios = async (id: string) => {
   const { portfolios } = await data
@@ -11,7 +13,8 @@ const fetchPortfolios = async (id: string) => {
 
 const PortfolioPage = async ({ params }: { params: { id: string } }) => {
   const { id } = params
-  const portfolio = await fetchPortfolios(id)
+  // const portfolio = await fetchPortfolios(id)
+  const portfolio = await fetchData(`${baseUrl}/api/portfolios/${id}`)
   if (!portfolio) {
     notFound()
   }
--- src/prisma/portfolios.ts
+++ src/prisma/portfolios.ts
@@ -4,3 +4,14 @@ export const getAllPortfolios = async () => {
   const portfolios = await prisma.portfolio.findMany()
   return portfolios
 }
+
+export const getSinglePotfolio = async (id: string) => {
+  // const body = req.json()
+  const portfolio = await prisma.portfolio.findUnique({
+    where: {
+      id: id
+    }
+  })
+  return portfolio
+  // return body
+}
