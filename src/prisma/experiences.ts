3c7c457c232c58f6ef1af2e885f1c539dc1fdafe
Lester <32542078+lhes23@users.noreply.github.com>
2023-08-03 12:44:36 +0800

    sort job experiences in desc order

--- src/prisma/experiences.ts
+++ src/prisma/experiences.ts
@@ -1,6 +1,10 @@
 import { prisma } from "./prismaInstantiate"
 
 export const getAllExperiences = async () => {
-  const experiences = await prisma.experience.findMany()
+  const experiences = await prisma.experience.findMany({
+    orderBy: {
+      workDate: "desc"
+    }
+  })
   return experiences
 }
