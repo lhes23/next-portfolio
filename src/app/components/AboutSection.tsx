edc0e21c7d1a45a13c603916ef768889a61a63e1
Lester <32542078+lhes23@users.noreply.github.com>
2024-02-17 21:06:07 +0800

    add meteors and earth together

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -4,6 +4,22 @@ import { motion, useInView } from "framer-motion"
 // import { slideFromLeft, scaleUp } from "@/utils/animations"
 import EarthCanvas from "./canvas/Earth"
 import { styles } from "./MainContent"
+import { Meteors } from "./ui/meteors"
+
+export function MeteorsEarth() {
+  return (
+    <div className="">
+      <div className="h-full md:h-1/2 w-full  relative">
+        <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+        <div className="relative  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <EarthCanvas />
+          {/* Meaty part - Meteor effect */}
+          <Meteors number={20} />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const AboutSection = ({ summary }: { summary: string }) => {
   // const aboutRef = useRef<HTMLDivElement>(null)
@@ -11,18 +27,9 @@ const AboutSection = ({ summary }: { summary: string }) => {
 
   return (
     // <section id="about" className={styles.section} ref={aboutRef}>
-    <section id="about" className={styles.section}>
-      <div className="grid grid-cols-1 gap-4 lg:gap-8 lg:grid-cols-2">
-        <motion.div
-          // variants={slideFromLeft}
-          initial="hidden"
-          // animate={isInView ? "visible" : "hidden"}
-          animate="visible"
-          // exit={!isInView ? "hidden" : ""}
-          className="relative h-64 overflow-hidden rounded-lg sm:h-full"
-        >
-          <EarthCanvas />
-        </motion.div>
+    <section id="about" className={`${styles.section} bg-gray-900`}>
+      <div className="grid grid-cols-1 gap-4 lg:gap-8 lg:grid-cols-2 ">
+        <MeteorsEarth />
 
         <motion.div
           className={`p-4 md:p-10 ${styles.bgCardGlass}`}
